516601fe6906162efddb6f33ca092f858747f572
fedorez <fedorez@mail.ru>
2023-08-18 21:18:15 +0300

    pressed/released events for SwiftUI button added

--- TransferDataWCA Watch App/ContentView.swift
+++ TransferDataWCA Watch App/ContentView.swift
@@ -17,12 +17,37 @@ struct ContentView: View {
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
             Text(model.messageText)
-            Button("Transfer to iPhone", action: model.pingPhone)
+            Button("Transfer to iPhone", action: { })
+                .modifier(PressActions(onPress: {
+                    // Do something on press...
+                    model.pingPhone()
+                }, onRelease: {
+                    // Do something on release...
+                    model.pingPhone()
+                }))
         }
         .padding()
     }
     
     
+}
+
+struct PressActions: ViewModifier {
+    var onPress: () -> Void
+    var onRelease: () -> Void
+    
+    func body(content: Content) -> some View {
+        content
+            .simultaneousGesture(
+                DragGesture(minimumDistance: 0)
+                    .onChanged({ _ in
+                        onPress()
+                    })
+                    .onEnded({ _ in
+                        onRelease()
+                    })
+            )
+    }
 }
 
 struct ContentView_Previews: PreviewProvider {
